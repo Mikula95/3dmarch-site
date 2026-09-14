d7afc229d1809495471592021737e03a681e607a
Bojan Mikulic <bojan.mikulic@supernovabih.ba>
2026-09-14 21:02:46 +0200

    Remove duplicated hero title from variousproductvizualisation.html

---
+++ fix_various.py
@@ -0,0 +1,11 @@
+﻿import re
+
+with open('variousproductvizualisation.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Remove the hero-imagetop section
+pattern = re.compile(r'<!-- Hero Image Top -->\s*<section class="hero-imagetop">.*?</section>', re.DOTALL)
+content = pattern.sub('', content)
+
+with open('variousproductvizualisation.html', 'w', encoding='utf-8') as f:
+    f.write(content)
